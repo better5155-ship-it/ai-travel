78dc3c9b367ba8a2e4b7ec0f5dc62d8fa2f3d785
better5155-ship-it <better5155-ship-it@users.noreply.github.com>
2026-05-23 00:58:16 +0900

    map modified

--- components/map/KakaoLoader.tsx
+++ components/map/KakaoLoader.tsx
@@ -29,7 +29,7 @@ export default function KakaoLoader() {
       "https://dapi.kakao.com/v2/maps/sdk.js?appkey=3d112e0ba8776ccde7a66d1867f26b39&autoload=false&libraries=services"
 
     script.async = true
-
+	script.crossOrigin = "anonymous"
     script.onload = () => {
       console.log("Kakao SDK loaded SUCCESS")
       console.log((window as any).kakao)
